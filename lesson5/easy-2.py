fc2c8dd82db0ba2328e9f881dfc21bd485519970
DaniilIssues <47916079+DaniilIssues@users.noreply.github.com>
2019-03-15 00:14:28 +0500

    1

--- lesson5/easy-2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-dir = input("Input directory:")
-
-for i in os.listdir(dir):
-    print(i)
--- lesson5/normal-1.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("N", type=int, help="Num")
-parser.add_argument("S", type=str, help="Text")
-args = parser.parse_args()
-for i in range(args.N):
-    print(args.S)
